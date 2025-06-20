601480dda1103bdd30eac6cd0d509c48c4f08d80
jose-guerrerog <joseguerrerog880@gmail.com>
2025-06-20 10:17:16 +1000

    fix endpoint

--- portfolio/services.py
+++ portfolio/services.py
@@ -144,7 +144,7 @@ class CoinGeckoService:
 
         params = {
             'ids': coin_ids_str,
-            'vs_currency': 'usd',
+            'vs_currencies': 'usd',
             'order': 'market_cap_desc',
             'per_page': 250,
             'page': 1,
